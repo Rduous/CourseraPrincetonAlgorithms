82ca43cd9c9774248aacc502e9b509eae44aa0e4
Rduous <git@Rduous.e4ward.com>
2015-02-13 14:19:23 -0700

    Tweak to Subset.java to pass bonus test

--- rQueueAndDeque/src/main/java/Subset.java
+++ rQueueAndDeque/src/main/java/Subset.java
@@ -4,11 +4,18 @@ public class Subset {
         int k = Integer.valueOf(args[0]);
         RandomizedQueue<String> strings = new RandomizedQueue<String>();
         String[] input = StdIn.readAllStrings();
-        for (String string : input) {
-            strings.enqueue(string);
-        }
         for (int i = 0; i < k; i++) {
-            System.out.println(strings.dequeue());
+            int r;
+            do {
+                r = StdRandom.uniform(input.length);
+            } while (input[r] == null);
+            String output = input[r];
+            input[r] = null;
+            strings.enqueue(output);
+        }
+        
+        for (String string : strings) {
+            System.out.println(string);
         }
     }
 }
