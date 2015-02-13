12de1ef4f67b5025b67427ca22aa3e6c852c4ad0
Rduous <git@Rduous.e4ward.com>
2015-02-13 13:38:34 -0700

    Cleanup and formatting

--- rQueueAndDeque/src/main/java/Deque.java
+++ rQueueAndDeque/src/main/java/Deque.java
@@ -1,121 +1,120 @@
 
-
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class Deque<Item> implements Iterable<Item> {
-	
-	private Node firstNode = null;
-	private Node lastNode = null;
-	private int size = 0;
 
-	public void addFirst(Item item) {
-		if (item == null) {
-			throw new NullPointerException();
-		}
-		Node node = new Node(item);
-		node.next = firstNode;
-		if (isEmpty()) {
-			lastNode = node;
-		} else {
-			firstNode.prev = node;
-		}
-		firstNode = node;
-		size++;
-	}
+    private Node firstNode = null;
+    private Node lastNode = null;
+    private int size = 0;
 
-	public void addLast(Item item) {
-		if (item == null) {
-			throw new NullPointerException();
-		}
-		Node node = new Node(item);
-		node.prev = lastNode;
-		if (isEmpty()) {
-			firstNode = node;
-		} else {
-			lastNode.next = node;
-		}
-		lastNode = node;
-		size++;
-	}
+    public void addFirst(Item item) {
+        if (item == null) {
+            throw new NullPointerException();
+        }
+        Node node = new Node(item);
+        node.next = firstNode;
+        if (isEmpty()) {
+            lastNode = node;
+        } else {
+            firstNode.prev = node;
+        }
+        firstNode = node;
+        size++;
+    }
 
-	public Item removeLast() {
-		if (isEmpty()) {
-			throw new NoSuchElementException();
-		}
-		Node oldLast = lastNode;
-		lastNode = oldLast.prev;
-		if (lastNode == null) {
-			firstNode = null;
-		} else {
-			lastNode.next = null;
-		}
-		size--;
-		return oldLast.item;
-	}
+    public void addLast(Item item) {
+        if (item == null) {
+            throw new NullPointerException();
+        }
+        Node node = new Node(item);
+        node.prev = lastNode;
+        if (isEmpty()) {
+            firstNode = node;
+        } else {
+            lastNode.next = node;
+        }
+        lastNode = node;
+        size++;
+    }
 
-	public Item removeFirst() {
-		if (isEmpty()) {
-			throw new NoSuchElementException();
-		}
-		Node oldFirst = firstNode;
-		firstNode = oldFirst.next;
-		if (firstNode != null) {
-			firstNode.prev = null;
-		} else {
-			lastNode = null;
-		}
-		size--;
-		return oldFirst.item;
-	}
+    public Item removeLast() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        Node oldLast = lastNode;
+        lastNode = oldLast.prev;
+        if (lastNode == null) {
+            firstNode = null;
+        } else {
+            lastNode.next = null;
+        }
+        size--;
+        return oldLast.item;
+    }
 
-	public boolean isEmpty() {
-		return size == 0;
-	}
-	
-	public Iterator<Item> iterator() {
-		// TODO Auto-generated method stub
-		return new DequeIterator();
-	}
+    public Item removeFirst() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        Node oldFirst = firstNode;
+        firstNode = oldFirst.next;
+        if (firstNode != null) {
+            firstNode.prev = null;
+        } else {
+            lastNode = null;
+        }
+        size--;
+        return oldFirst.item;
+    }
 
-	public int size() {
-		return size;
-	}
-	
-	private class Node {
-		Item item;
-		Node next;
-		Node prev;
-		public Node(Item item) {
-			this.item = item;
-		}
-	}
-	
-	private class DequeIterator implements Iterator<Item> {
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
-		Node next;
-		
-		public DequeIterator() {
-			next = firstNode;
-		}
-		
-		public boolean hasNext() {
-			return next != null;
-		}
+    public Iterator<Item> iterator() {
+        return new DequeIterator();
+    }
 
-		public Item next() {
-			if (! hasNext()) {
-				throw new NoSuchElementException();
-			}
-			Node prev = next;
-			next = prev.next;
-			return prev.item;
-		}
-		
-		public void remove() throws UnsupportedOperationException {
-			throw new UnsupportedOperationException();
-		}
-		
-	}
+    public int size() {
+        return size;
+    }
+
+    private class Node {
+        Item item;
+        Node next;
+        Node prev;
+
+        public Node(Item item) {
+            this.item = item;
+        }
+    }
+
+    private class DequeIterator implements Iterator<Item> {
+
+        Node next;
+
+        public DequeIterator() {
+            next = firstNode;
+        }
+
+        public boolean hasNext() {
+            return next != null;
+        }
+
+        public Item next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            Node prev = next;
+            next = prev.next;
+            return prev.item;
+        }
+
+        public void remove() throws UnsupportedOperationException {
+            throw new UnsupportedOperationException();
+        }
+
+    }
 
 }
--- rQueueAndDeque/src/main/java/RandomizedQueue.java
+++ rQueueAndDeque/src/main/java/RandomizedQueue.java
@@ -1,131 +1,131 @@
 
-
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
-	
-	protected static final int INITIAL_CAPACITY = 10;
-	protected static final double RESIZE_DOWN_RATIO = 0.25;
-	
-	private Item[] items = (Item[]) new Object[INITIAL_CAPACITY];
-	private int pointer = 0;
-	private int size = 0;
-	
-	public RandomizedQueue() {
-		
-	}
 
-	public boolean isEmpty() {
-		return size == 0;
-	}
+    protected static final int INITIAL_CAPACITY = 10;
+    protected static final double RESIZE_DOWN_RATIO = 0.25;
 
-	public int size() {
-		return size;
-	}
+    private Item[] items = (Item[]) new Object[INITIAL_CAPACITY];
+    private int pointer = 0;
+    private int size = 0;
 
-	public void enqueue(Item item) {
-		if (item == null) {
-			throw new NullPointerException();
-		}
-		if (needToResize()) {
-			resize();
-		}
-		items[pointer++] = item;
-		size++;
-	}
+    public RandomizedQueue() {
 
-	public Item dequeue() {
-		if (isEmpty()) {
-			throw new NoSuchElementException();
-		}
-		
-		int toReturn = getRandomItemPointer();
-		Item item = items[toReturn];
-		items[toReturn] = null;
-		size--;
-		if (needToCompress()) {
-			compress();
-		}
-		return item;
-	}
+    }
 
-	private int getRandomItemPointer() {
-		int toReturn;
-		do { 
-			toReturn = StdRandom.uniform(pointer);
-		}
-		while (items[toReturn] == null);
-		return toReturn;
-	}
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
-	public Item sample() {
-		if (isEmpty()) {
-			throw new NoSuchElementException();
-		}
-		return items[getRandomItemPointer()];
-	}
+    public int size() {
+        return size;
+    }
 
-	public Iterator<Item> iterator() {
-		return new RQIter();
-	}
+    public void enqueue(Item item) {
+        if (item == null) {
+            throw new NullPointerException();
+        }
+        if (needToResize()) {
+            resize();
+        }
+        items[pointer++] = item;
+        size++;
+    }
 
-	protected boolean needToResize() {
-		return  pointer == items.length; 
-	}
-	
-	protected boolean needToCompress() {
-		return items.length > INITIAL_CAPACITY && ((double) size) / items.length < RESIZE_DOWN_RATIO;
-	}
+    public Item dequeue() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
 
-	protected void resize() {
-		Item[] oldItems = items;
-		items = (Item[]) new Object[items.length * 2];
-		System.arraycopy(oldItems, 0, items, 0, oldItems.length);
-	}
-	
-	protected void compress() {
-		Item[] newItems = (Item[]) new Object[items.length / 2];
-		int copied = 0;
-		for (int i = 0; copied < size && i < pointer; i++) {
-			if (items[i] != null) {
-				newItems[copied++] = items[i];
-			}
-		}
-		items = newItems;
-		pointer = size + 1;
-	}
-	
-	private class RQIter implements Iterator<Item> {
-		
-		Item[] items;
-		int size;
-		int pointer = 0;
-		
-		public RQIter() {
-			items = Arrays.copyOf(RandomizedQueue.this.items, RandomizedQueue.this.pointer);
-			StdRandom.shuffle(items);
-			size = RandomizedQueue.this.size;
-		}
+        int toReturn = getRandomItemPointer();
+        Item item = items[toReturn];
+        items[toReturn] = null;
+        size--;
+        if (needToCompress()) {
+            compress();
+        }
+        return item;
+    }
 
-		public boolean hasNext() {
-			return size != 0;
-		}
+    private int getRandomItemPointer() {
+        int toReturn;
+        do {
+            toReturn = StdRandom.uniform(pointer);
+        } while (items[toReturn] == null);
+        return toReturn;
+    }
 
-		public Item next() {
-			if (!hasNext()) {
-				throw new NoSuchElementException();
-			}
-			while (items[pointer] == null) {
-				pointer++;
-			}
-			size--;
-			return items[pointer++];
-		}
-		
-		public void remove() throws UnsupportedOperationException {
-			throw new UnsupportedOperationException();
-		}
-	}
+    public Item sample() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return items[getRandomItemPointer()];
+    }
+
+    public Iterator<Item> iterator() {
+        return new RQIter();
+    }
+
+    protected boolean needToResize() {
+        return pointer == items.length;
+    }
+
+    protected boolean needToCompress() {
+        return items.length > INITIAL_CAPACITY
+                && ((double) size) / items.length < RESIZE_DOWN_RATIO;
+    }
+
+    protected void resize() {
+        Item[] oldItems = items;
+        items = (Item[]) new Object[items.length * 2];
+        System.arraycopy(oldItems, 0, items, 0, oldItems.length);
+    }
+
+    protected void compress() {
+        Item[] newItems = (Item[]) new Object[items.length / 2];
+        int copied = 0;
+        for (int i = 0; copied < size && i < pointer; i++) {
+            if (items[i] != null) {
+                newItems[copied++] = items[i];
+            }
+        }
+        items = newItems;
+        pointer = size + 1;
+    }
+
+    private class RQIter implements Iterator<Item> {
+
+        Item[] items;
+        int size;
+        int pointer = 0;
+
+        public RQIter() {
+            items = Arrays.copyOf(RandomizedQueue.this.items,
+                    RandomizedQueue.this.pointer);
+            StdRandom.shuffle(items);
+            size = RandomizedQueue.this.size;
+        }
+
+        public boolean hasNext() {
+            return size != 0;
+        }
+
+        public Item next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            while (items[pointer] == null) {
+                pointer++;
+            }
+            size--;
+            return items[pointer++];
+        }
+
+        public void remove() throws UnsupportedOperationException {
+            throw new UnsupportedOperationException();
+        }
+    }
 }
