65124722eaf8fe321d2666b55381c08252d103e7
Rduous <git@Rduous.e4ward.com>
2015-02-18 14:56:40 -0700

    Fixes problem with already-seen endpoints
    Kind of a subtle one. I'm identifying 'already-seen' segments (i.e. subsegments) by tracking which endpoints I've already seen.  But two separate line segments can share the same endpoint; in that corner case, the second line segment wasn't getting printed. Not sure if the API checker of the assignment will allow this inner class -- hope so.

--- linePatterns/src/main/java/Fast.java
+++ linePatterns/src/main/java/Fast.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 
 public class Fast {
@@ -28,7 +29,7 @@ public class Fast {
     static void findAndPrintLineSegments(Point[] naturalOrderPoints) {
         int n = naturalOrderPoints.length;
        
-        List<Point> endPoints = new ArrayList<Point>();
+        List<PointPair> endPoints = new ArrayList<PointPair>();
         for (int i = 0; i < n-3; i++) {
             Point p = naturalOrderPoints[i];
             Point[] points = Arrays.copyOfRange(naturalOrderPoints, i+1,n);
@@ -58,21 +59,49 @@ public class Fast {
     }
 
     private static void printAndDrawLineSegment(Point startPoint, Point[] otherPoints, int sequenceStartIndex,
-            int numPoints, List<Point> endPoints) {
+            int numPoints, List<PointPair> endPoints) {
 
         //check that this isn't a smaller subsegment
         Point[] toPrint = Arrays.copyOfRange(otherPoints, sequenceStartIndex, sequenceStartIndex+numPoints);
         Arrays.sort(toPrint);
         Point endPoint = toPrint[numPoints-1];
-        if (! endPoints.contains(endPoint)) {
+        PointPair segmentEndPoints = new PointPair(toPrint[numPoints-2], endPoint);
+        if (! endPoints.contains(segmentEndPoints)) {
             System.out.print(startPoint + " -> ");
             for (int i = 0; i < numPoints-1; i++) {
                 System.out.print(toPrint[i] + " -> ");
             }
             System.out.println(endPoint);
             startPoint.drawTo(endPoint);
-            endPoints.add(endPoint);
+            endPoints.add(segmentEndPoints);
         }
     }   
+    
+    private static class PointPair {
+        Point p1;
+        Point p2;
+        
+        public PointPair(Point p1, Point p2) {
+            this.p1 = p1;
+            this.p2 = p2;
+        }
+        
+        @Override
+        public int hashCode() {
+            return Objects.hash(p1, p2);
+        }
+        
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == null) {
+                return false;
+            }
+            if (! (obj instanceof PointPair)) {
+                return false;
+            }
+            PointPair other = (PointPair) obj;
+            return Objects.equals(p1, other.p1) && Objects.equals(p2, other.p2);
+        }
+    }
 
 }
